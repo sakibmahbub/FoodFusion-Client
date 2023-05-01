dca2930dd653c8df2292091c116c5fbfc1ce2864
sakibmahbub <arafatsakib48@gmail.com>
2023-05-02 01:11:29 +0600

    created footer

--- src/pages/Shared/Footer.jsx
+++ src/pages/Shared/Footer.jsx
@@ -1,7 +1,50 @@
 import React from "react";
 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <div className="my-container">
+      <footer className="footer sm:ms-10 bg-gray-50 p-10 text-base-content">
+        <div>
+          <span className="footer-title">Cuisine</span>
+          <a className="link link-hover">Fast Food</a>
+          <a className="link link-hover">Salad</a>
+          <a className="link link-hover">Drinks</a>
+          <a className="link link-hover">Steak</a>
+        </div>
+        <div>
+          <span className="footer-title">Company</span>
+          <a className="link link-hover">About us</a>
+          <a className="link link-hover">Contact</a>
+          <a className="link link-hover">Branches</a>
+          <a className="link link-hover">FAQ</a>
+        </div>
+        <div>
+          <span className="footer-title">Legal</span>
+          <a className="link link-hover">Terms of use</a>
+          <a className="link link-hover">Privacy policy</a>
+          <a className="link link-hover">Cookie policy</a>
+        </div>
+        <div>
+          <span className="footer-title">Newsletter</span>
+          <div className="form-control w-80">
+            <label className="label">
+              <span className="label-text">Subscribe to our Newsletter</span>
+            </label>
+            <div className="relative">
+              <input
+                type="text"
+                placeholder="your email address"
+                className="input input-bordered w-full pr-16"
+              />
+              <button className="btn btn-secondary absolute top-0 right-0 rounded-l-none">
+                Subscribe
+              </button>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
 };
 
 export default Footer;
--- src/pages/Shared/Header.jsx
+++ src/pages/Shared/Header.jsx
@@ -5,8 +5,8 @@ import { Link, NavLink } from "react-router-dom";
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
-    <div className=" px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 relative z-20">
-      <div className="relative flex items-center justify-around">
+    <div className=" px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-24 relative z-20">
+      <div className="relative flex items-center justify-between">
         {/* Logo Section */}
         <Link to="/" className="inline-flex items-center">
           <span className="ml-2 text-3xl font-bold tracking-wide text-gray-800">
