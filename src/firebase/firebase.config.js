53c1ddc56a34751a6c4ddc5ba4bd1a9b81697aa3
sakibmahbub <arafatsakib48@gmail.com>
2023-05-02 00:32:09 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCHXyp6O7YoZ1hidue0uPi5u9smB3oE3HY",
+  authDomain: "food-fusion-a8a59.firebaseapp.com",
+  projectId: "food-fusion-a8a59",
+  storageBucket: "food-fusion-a8a59.appspot.com",
+  messagingSenderId: "484243161331",
+  appId: "1:484243161331:web:caa02192a668859028b786",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
---
+++ src/pages/Shared/Header.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import { Bars3BottomRightIcon, XMarkIcon } from "@heroicons/react/24/solid";
+import { Link, NavLink } from "react-router-dom";
+
+const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  return <div></div>;
+};
+
+export default Header;
