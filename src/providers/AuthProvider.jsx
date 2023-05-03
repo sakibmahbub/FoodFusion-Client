95f4a235d5f655fa798d5b94ff54df6dc470a2d2
sakibmahbub <arafatsakib48@gmail.com>
2023-05-03 16:03:21 +0600

    displayed spinner on loading state

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -13,22 +13,27 @@ const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   const createUser = (email, password) => {
+    setLoading(true);
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
   const logIn = (email, password) => {
+    setLoading(true);
     return signInWithEmailAndPassword(auth, email, password);
   };
 
   const logOut = () => {
+    setLoading(true);
     return signOut(auth);
   };
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (loggedUser) => {
       setUser(loggedUser);
+      setLoading(false);
     });
 
     return () => {
@@ -36,7 +41,7 @@ const AuthProvider = ({ children }) => {
     };
   }, []);
 
-  const authInfo = { user, createUser, logIn, logOut };
+  const authInfo = { user, createUser, logIn, logOut, loading };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
   );
